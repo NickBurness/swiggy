8ee6037871c74fde9fc9c41c984a00c13e8ffaef
NickBurness <Nickburness93@outlook.com>
2020-09-22 19:36:50 +0100

    replace single quotes with double quotes except where template literals are present

--- commands/rng.js
+++ commands/rng.js
@@ -1,6 +1,6 @@
 module.exports = {
-	name: 'rng',
-    description: 'Generate a random number between 1 and 28!',
+	name: "rng",
+    description: "Generate a random number between 1 and 28!",
 	execute(message, args) {
         function getRandomInt() {
             return Math.floor(Math.random() * Math.floor(28));
--- commands/user-info.js
+++ commands/user-info.js
@@ -1,6 +1,6 @@
 module.exports = {
-	name: 'user-info',
-	description: 'User Info!',
+	name: "user-info",
+	description: "User Info!",
 	execute(message, args) {
 		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
 	},
