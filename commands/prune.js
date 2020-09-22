628add6df6abb4b76d27f1d911e9ef1a344d0b2b
NickBurness <Nickburness93@outlook.com>
2020-09-22 19:32:59 +0100

    parathenses added to arrow function

--- commands/prune.js
+++ commands/prune.js
@@ -10,7 +10,7 @@ module.exports = {
 			return message.reply("you need to input a number between 1 and 99.");
 		}
 
-		message.channel.bulkDelete(amount, true).catch(err => {
+		message.channel.bulkDelete(amount, true).catch((err) => {
 			console.error(err);
 			message.channel.send("there was an error trying to prune messages in this channel!");
 		});
