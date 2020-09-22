075b15c3f2a982cf6aa4a42b688d4c15b9fa481c
NickBurness <Nickburness93@outlook.com>
2020-09-22 19:34:22 +0100

    replace single quotes with double quotes

--- commands/quest.js
+++ commands/quest.js
@@ -1,28 +1,28 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports = {
-    name: 'quest',
-    description: 'tell swiggy to generate an embedded link to the Old School Runescape Wiki quest guide passed in as an argument',
+    name: "quest",
+    description: "tell swiggy to generate an embedded link to the Old School Runescape Wiki quest guide passed in as an argument",
     execute(message, args) {
-        let quest = args.toString().replace(/,/g, '_');
+        let quest = args.toString().replace(/,/g, "_");
         const exampleEmbed = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle(`OSWiki - ${quest}`)
-        .setURL(`https://oldschool.runescape.wiki/${quest}`)
-        .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-        .setDescription('Some description here')
-        .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-        .addFields(
-            { name: 'Regular field title', value: 'Some value here' },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-        )
-        .addField('Inline field title', 'Some value here', true)
-        .setImage('https://i.imgur.com/wSTFkRM.png')
-        .setTimestamp()
-        .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-    
-    message.channel.send(exampleEmbed);
+            .setColor("#0099ff")
+            .setTitle(`OSWiki - ${quest}`)
+            .setURL(`https://oldschool.runescape.wiki/${quest}`)
+            .setAuthor("Some name", "https://i.imgur.com/wSTFkRM.png", "https://discord.js.org")
+            .setDescription("Some description here")
+            .setThumbnail("https://i.imgur.com/wSTFkRM.png")
+            .addFields(
+                { name: "Regular field title", value: "Some value here" },
+                { name: "\u200B", value: "\u200B" },
+                { name: "Inline field title", value: "Some value here", inline: true },
+                { name: "Inline field title", value: "Some value here", inline: true },
+            )
+            .addField("Inline field title", "Some value here", true)
+            .setImage("https://i.imgur.com/wSTFkRM.png")
+            .setTimestamp()
+            .setFooter("Some footer text here", "https://i.imgur.com/wSTFkRM.png");
+
+        message.channel.send(exampleEmbed);
     },
 };
