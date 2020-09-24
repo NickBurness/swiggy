c0a60d45e066162abaeb24030040a21c54c2f747
NickBurness <Nickburness93@outlook.com>
2020-09-25 00:16:58 +0100

    create voice command using streamDispatcher

---
+++ commands/wow.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "wow",
+    description: "play wow sound in voice channel",
+    execute(message, args) {
+        if (message.member.voice.channel) {
+            message.member.voice.channel.join().then(connection => {
+                const dispatcher = connection.play('audio/anime-wow-sound-effect.mp3');
+                dispatcher.on("end", () => {
+                    this.message.channel.send('Finished Playing.')
+                    dispatcher.end();
+                    this.message.member.voice.channel.leave();
+                })
+            })
+        } else {
+            message.reply('You need to join a voice channel first!');
+        }
+    }
+};
